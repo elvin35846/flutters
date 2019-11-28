ca54370f59679ecb912fd26e9082002c1f6c495b
Elvin <33036680+elvin35846@users.noreply.github.com>
2019-11-28 12:18:18 +0400

    1

--- dart/main2.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:math';
-main() {
-  var pi2 = pi;
-    var s = 'Merhaba Pi=';
-        var object = s+ pi2.toString();
-                print(object);
-}
--- flutter_app/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import './arayuz/MainPage.dart' show MainPage;
-import './arayuz/Hello.dart' show Hello;
-
-void main() {
-  runApp(new Hello());
-}
