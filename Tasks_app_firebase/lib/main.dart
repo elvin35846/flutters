90d4577f5dbcf20d649edea71fbfeef8b4625485
Elvin Alishov <elvin.marmocer@gmail.com>
2021-02-26 13:22:20 +0400

    fix

---
+++ Tasks_app_firebase/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'tasksList.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tasks app',
+      debugShowCheckedModeBanner: false,
+      home: TasksList(),
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+    );
+  }
+}
+
+
